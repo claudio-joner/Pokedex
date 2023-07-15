ccd643103fd264c05a8baed1b27815dc417ec512
claudio-joner <claujoner@gmail.com>
2023-07-15 09:17:26 -0300

    Se agregan Clases al proyecto

---
+++ Negocio/AccecsoDatos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace Negocio
+{
+    public class AccecsoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+        private string cadenaConexion = "Data Source=DESKTOP-6KIGGOG\\MSSQLSERVER01;Initial Catalog=POKEDEX_DB;Integrated Security=True; Encrypt=False";
+
+        public SqlDataReader Lector { get => lector; }
+
+        public AccecsoDatos()
+        {
+            conexion = new SqlConnection(cadenaConexion);
+            comando = new SqlCommand();    
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            if(lector != null)
+            {
+                lector.Close();
+            }
+            conexion.Close();
+        }
+    }
+}
---
+++ Negocio/ElementoNegocio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class ElementoNegocio
+    {
+        public List<Elemento> listar()
+        {
+			List<Elemento> lista = new List<Elemento>();
+			AccecsoDatos datos = new AccecsoDatos();
+			string consulta = "Selec * from Elementos";
+			try
+			{
+				datos.setearConsulta(consulta);
+				datos.ejecutarLectura();
+
+				while(datos.Lector.Read())
+				{
+					Elemento aux = new Elemento();
+					aux.Id = (int)datos.Lector[0];
+					aux.Descripcion = (string)datos.Lector[1];
+				}
+
+				return lista;
+			}
+			catch (Exception ex)
+			{
+
+				throw ex;
+			}
+			finally
+			{
+				datos.CerrarConexion();
+			}
+        }
+    }
+}
